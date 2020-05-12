6372420cd7b5d14df1195dd93866d9d7d36bb4d0
cezary986 <cezary.maszczyk@gmail.com>
2020-05-12 15:43:20 +0200

    Add sample java project to test gradle integration

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public void sampleMethod() {
+        System.out.println("do something...");
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello word");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void sampleMethod() {
+        System.out.println("run sampleMethod test");
+        Assert.assertTrue( false);
+    }
+}
